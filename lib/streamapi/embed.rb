4a86b63cc74cedefb0da0f095ccf324fa6a4fd33
Panagiotis Papadopoulos <panosjee@gmail.com>
2010-11-09 11:58:46 +0200

    fixed embed code

--- lib/streamapi/embed.rb
+++ lib/streamapi/embed.rb
@@ -3,49 +3,27 @@ module StreamAPI
 
     def self.to_html(private_host_id, public_host_id, site_id, theme)
       embed_code = <<-EOF
-      <OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" 
-      codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" 
-      width="#{theme['width']}" height="#{theme['height']}" id="liveStream" align="">
-      <PARAM name="movie" value="http://static.streamapi.com/flash/loader.swf?app=custom.swf&1247246891974">
-      <PARAM name="FlashVars" value="siteID=#{site_id}&publicHostID=#{public_host_id}
-      &layoutPath=#{theme['layout_path']}
-      &skinPath=#{theme['skin_path']}
-      &privateHostID=#{theme['private_host_id']}&userType=host&">
-      <PARAM name="allowScriptAccess" value="always">
-      <PARAM name="allowFullScreen" value="true">
-      <PARAM name="quality" value="high">
-      <PARAM name="bgcolor" value="#000000">
-      <PARAM name="scale" value="noscale">
-      <PARAM name="wmode" value="transparent">
-      <embed src="http://static.streamapi.com/flash/loader.swf?app=custom.swf&1247246891974" 
-      FlashVars="siteID=#{site_id}&publicHostID=#{public_host_id}
-      &layoutPath=#{theme['layout_path']}
-      &skinPath=#{theme['skin_path']}
-      &privateHostID=#{theme['private_host_id']}&userType=host&" 
-      allowScriptAccess="always" allowFullScreen="true" width="#{theme['width']}" height="#{theme['height']}" scale="noscale" 
-      wmode="transparent" quality="high" bgcolor="#000000" type="application/x-shockwave-flash" >
-      </embed>
-      </OBJECT>
-          <div id="player_container"></div>
-
-          <script type="text/javascript" 
-          	src="http://ajax.googleapis.com/ajax/libs/swfobject/2.1/swfobject.js"></script>
-
-          <script type="text/javascript">
-             swfobject.embedSWF("http://static.streamapi.com/flash/loader.swf", "player_container", 
-             						"#{theme['width']}", "#{theme['height']}", "9", "", {
-                  'app' : 'custom.swf',
-                  'siteID' : '#{site_id}',
-                  'publicHostID' : '#{public_host_id}',
-                  'layoutPath' : '#{theme['layout_path']}',
-                  'skinPath' : '#{theme['skin_path']}',
-                  'privateHostID' : '#{theme['private_host_id']}',
-                  'userType' : 'host'
-              }, {
-                  allowfullscreen: "true",
-                  allowscriptaccess: "true"
-              });
-          </script type="text/javascript">
+        <embed 
+      		height="#{theme['height'] }" 
+      		width="#{theme['width'] }" 
+      		wmode="" 
+      		type="application/x-shockwave-flash" 
+      		bgcolor="#000000" 
+      		quality="high" 
+      		scale="noscale" 
+      		allowfullscreen="true" 
+      		allowscriptaccess="always" 
+      		flashvars=
+      		"authField2Label=Display Name:
+      		&authField1Label=UserName:
+      		&siteID=#{site_id}
+      		&layoutPath=#{theme['layout_path']}
+      		&skinPath=#{theme['skin_path']}
+      		&publicHostID=#{public_host_id}
+      		&privateHostID=#{private_host_id}
+      		&authType=key
+      		&userType=204&" 
+      		src="http://static.streamapi.com/flash/loader.swf?app=custom.swf" />
       EOF
       
       embed_code
